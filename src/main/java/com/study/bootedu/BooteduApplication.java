b43b0e1e6165140e11547a4a391140ef4c203f98
이경용 <sleekydz86@icloud.com>
2023-12-05 17:59:09 +0900

    SpringBoot_edu_restClient 셋팅
    SpringBoot_edu_restClient

---
+++ src/main/java/com/study/bootedu/BooteduApplication.java
@@ -0,0 +1,13 @@
+package com.study.bootedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooteduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooteduApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/bootedu/BooteduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.bootedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooteduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
